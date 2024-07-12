91a91ed34d801e5819ad76c96f3238e207ed00e0
MarianoWWW <marianoworoszylo@hotmail.com>
2024-07-12 08:38:13 -0300

    Add files via upload

---
+++ B01.s
@@ -0,0 +1,13 @@
+ #B.01 Escriba la instrucción necesaria en Ripes para lograr que el registro x5 tenga el valor
+ #hexadecimal 0x000003FF (note que 3FF se codifica con menos de 12 bits)
+ 
+ .text
+ 
+     li x5,0x3ff
+     
+ 
+ 
+ end:
+     beq x0,x0,end
+     
+    
