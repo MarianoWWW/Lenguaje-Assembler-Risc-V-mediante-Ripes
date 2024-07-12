f26e97fc58ea59ac3caec405b866241ad98ffe19
MarianoWWW <marianoworoszylo@hotmail.com>
2024-07-12 08:41:35 -0300

    Add files via upload

---
+++ B02.s
@@ -0,0 +1,9 @@
+·# B.02 Escriba las instrucciones necesarias en Ripes para lograr que el registro x5 tenga el
+ # valor hexadecimal 0x12345678
+ 
+ .text
+ 
+     li x5,0x12345678
+     
+ end:
+     beq x0,x0,end
